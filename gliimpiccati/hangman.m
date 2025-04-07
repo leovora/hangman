ea05faa12f1f003422375e795e5048d705f39719
Alexandru Nicolescu <alexandru.nicolescu06@gmail.com>
2025-04-07 10:46:25 +0200

    package init

--- gliimpiccati/hangman.m
+++ gliimpiccati/hangman.m
@@ -1,3 +1,65 @@
 (* ::Package:: *)
 
+(* :Title: HangmanGame *)
+(* :Context: HangmanGame` *)
+(* :Author: GS *)
+(* :Summary: funzioni utilizzate nell'interfaccia hangman.nb *)
+(* :Copyright: GS 2025 *)
+(* :Package Version: 3 *)
+(* :Mathematica Version: 14 *)
+(* :History: last modified 07/04/2025 *)
+(* :Sources: bblio *)
+(* :Limitations: educational purposes *)
+(* :Discussion: *)
+(* :Requirements: *)
+(* :Warning: DOCUMENTATE TUTTO il codice *)
 
+BeginPackage["HangmanGame`"]
+
+GeneraEsercizio::usage = ""
+
+Seed::usage = ""
+
+MostraSoluzione::usage = ""
+
+VerificaRisultato::usage = ""
+
+Pulisci::usage = ""
+
+
+Begin["`Private`"]
+
+
+GeneraEsercizio[] :=
+Module[ {}, 
+];
+
+
+Seed[] :=
+Module[ {}, 
+];
+
+
+ MostraSoluzione[] :=
+Module[ {}, 
+];
+
+
+VerificaRisultato[] :=
+Module[ {}, 
+];
+
+
+Pulisci[] :=
+Module[ {}, 
+];
+
+
+(* Funzioni ausiliarie *)
+
+
+
+End[]
+
+
+EndPackage[]
