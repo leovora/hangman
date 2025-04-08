81e0dc56af8f72b3a431ecb64e5925c07af5b7fa
Alexandru Nicolescu <alexandru.nicolescu06@gmail.com>
2025-04-08 12:51:04 +0200

    added GeneraEsercizio function

--- gliimpiccati/hangman.m
+++ gliimpiccati/hangman.m
@@ -16,23 +16,46 @@
 
 BeginPackage["HangmanGame`"]
 
-GeneraEsercizio::usage = ""
+GeneraEsercizio::usage = "La funzionalit\[AGrave] Genera Esercizio prevede un generatore casuale di un numero potenzialmente infinito di
+esercizi. Date all\[CloseCurlyQuote]utente la possibilit\[AGrave] di generare un esercizio e/o rigenerare l\[CloseCurlyQuote]ultimo esercizio corrente, chiedendo un
+numero (Seed), ad esempio tramite finestra di input o checkbox."
 
-Seed::usage = ""
+MostraSoluzione::usage = " La funzionalit\[AGrave] Mostra Soluzione pu\[OGrave] essere resa disponibile anche tramite finestra di pop-up."
 
-MostraSoluzione::usage = ""
+VerificaRisultato::usage = "La funzionalit\[AGrave] Verifica Risultato serve ad aiutare l\[CloseCurlyQuote]utente ad uscire da una eventuale impasse."
 
-VerificaRisultato::usage = ""
+Pulisci::usage = "La funzionalit\[AGrave] Pulisci riporta l\[CloseCurlyQuote]interfaccia allo stato iniziale, ripulendo anche eventuali celle create (ad
+esempio, da Mostra Soluzione e/o Verfica Risultato) durante l\[CloseCurlyQuote]uso della interfaccia stessa"
 
-Pulisci::usage = ""
+GeneraInterfaccia::usage = "GeneraInterfaccia[]
+	Funzione che permette di generare un'interfaccia interattiva e dinamica, la quale richiama
+	le altre funzionalita' del gioco."
 
 
 Begin["`Private`"]
 
 
-GeneraEsercizio[] :=
-Module[ {}, 
+(* Implementazione della funzione GeneraEsericizio *)
+GeneraEsercizio[ seed_:RandomInteger[9999999], gamemode_:1 ] :=
+Module[ { wordlist, pattern, wordlen, word }, 
+	(* Imposta la lunghezza delle possibili parole in base alla difficolta' selezionata *)
+	If[ gamemode === 1, wordlen = {0,5} ];
+	If[ gamemode === 2, wordlen = {6,8} ];
+	If[ gamemode === 3, wordlen = {9,12} ];
+	
+	(* Recupera la lista di parole dal dizionario italiano filtrando in base alla lunghezza e i caratteri non consentiti *)
+	wordlist = Select[DictionaryLookup[{"Italian","*"}], Between[wordlen][StringLength[#]] && !HaCaratteriNonAmmessiQ[#] &];
+	
+	(* Imposta il seed per la selezione pseudo casuale *)
+	SeedRandom[seed];
+	
+	(* Selezione pseudo casuale di una parola dalla lista *)
+	word = RandomChoice[wordlist];
+	
+	(* Ritorno della parola selezionata come lista di caratteri *)
+	Characters[word]
 ];
+GeneraEsercizio[] (* Chiamata di funzione lasciata per le prove *)
 
 
 Seed[] :=
@@ -55,8 +78,16 @@ Module[ {},
 ];
 
 
-(* Funzioni ausiliarie *)
+(* Implementazione della funzione GeneraInterfaccia *)
+GeneraInterfaccia[]:= DynamicModule[{},
+	Manipulate[]]
 
+
+(* Funzioni ausiliarie *)
+HaCaratteriNonAmmessiQ[ s_ ] :=
+Module[ { accenti = {"\[AGrave]", "\[EGrave]", "\[IGrave]", "\[OGrave]", "\[UGrave]", "\[AAcute]", "\[EAcute]", "\[IAcute]", "\[OAcute]", "\[UAcute]"} },
+	StringContainsQ[s, Alternatives @@ accenti] || StringContainsQ[s, "'"]
+];
 
 
 End[]
