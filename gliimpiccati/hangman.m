f1bb731d770598fa25e0796307048d0c54c0eef2
AlexandruNicolescu00 <alexandru.nicolescu06@gmail.com>
2025-05-17 12:24:10 +0200

    more comments on package

--- gliimpiccati/hangman.m
+++ gliimpiccati/hangman.m
@@ -24,34 +24,43 @@ Begin["`Private`"];
 
 
 (* Implementazione della funzione GeneraEsericizio *)
-GeneraEsercizio[ gamemode_:1, seed_:Automatic ] :=
-Module[ { wordlist, pattern, wordlen, word, stato, errors={}, score=0 }, 
-	(* Imposta la lunghezza delle possibili parole in base alla difficolta' selezionata *)
-	If[ gamemode === 1, wordlen = {1,5} ];
-	If[ gamemode === 2, wordlen = {6,8} ];
-	If[ gamemode === 3, wordlen = {9,15} ];
+GeneraEsercizio[ gamemode_:1, seed_:Automatic ] := Module[ 
+{ 
+	wordlist, (* Lista delle possibili parole *)
+	wordlen, (* Lunghezza della parola *)
+	word, (* Parola da indovinare *)
+	stato, (* Vettore di trattini bassi *)
+	errors={}, (* Lista di errori *)
+	score=0 (* Punteggio *)
+}, 
+	(* Imposta la lunghezza delle possibili parole in base alla difficolt\[AGrave] selezionata *)
+	If[ gamemode === 1, wordlen = {1,5} ]; (* 1 = facile *)
+	If[ gamemode === 2, wordlen = {6,8} ]; (* 2 = media *)
+	If[ gamemode === 3, wordlen = {9,15} ]; (* 3 = difficile *)
 	
-	(* Recupera la lista di parole dal dizionario italiano filtrando in base alla lunghezza e i caratteri non consentiti *)
+	(* Filtra il dizionario italiano per parole della lunghezza desiderata e senza caratteri non ammessi *)
 	wordlist = Select[DictionaryLookup[{"Italian","*"}], Between[wordlen][StringLength[#]] && !HaCaratteriNonAmmessiQ[#] &];
 	
-	(* Imposta il seed per la selezione pseudo casuale *)
+	(* Imposta il seed per la selezione pseudo-casuale *)
 	SeedRandom[seed];
 	
-	(* Selezione pseudo casuale di una parola dalla lista *)
+	(* Estrae una parola casuale e la scompone in caratteri *)
 	word = Characters[RandomChoice[wordlist]];
 	
-	(* Inizializzazione dello stato *)
-	stato = InizializzaStato[ToLowerCase[word]];
+	(* Inizializzazione dello stato di gioco con trattini bassi per ogni lettera *)
+	stato = InizializzaStato[word];
 	
-	(* Ritorno della parola selezionata come lista di caratteri, lo stato iniziale e una lista vuota di errori *)
-	{word, stato, errors, score}
+	(* Ritorna il vettore lettere (in minuscolo), lo stato, la lista vuota di errori e punteggio a zero *)
+	{ToLowerCase[word], stato, errors, score}
 ];
 
 
-(* Implementazione della funzione MostraSoluzione *)
-MostraSoluzione[soluzione_List] := 
-Module[{stringa},
-	(* Concatenazione della lista di caratteri in una stringa mostrare all'utente *)
+(* Mostra la selezione in un pop-up *)
+MostraSoluzione[soluzione_List] := Module[
+{
+	stringa (* Soluzione da mostrare *)
+},
+	(* Concatenazione della lista di caratteri in una stringa da mostrare all'utente *)
 	stringa = StringJoin[soluzione];
 	
 	(* Messaggio da mostrare all'utente attraverso un pop-up *)
@@ -64,26 +73,34 @@ Module[{stringa},
 ];
 
 
-(* Implementazione della funzione suggerimento *)
-Suggerimento[word_List, stato_List, errors_List, score_Integer, gameMode_Integer] :=
-Module[ {suggerimento, lettereMancanti, newScore},
-	(* Recupero delle lettere mancanti tramite una sottrazione insiemistica *)
+(* Fornisce un suggerimento e penalizza il punteggio *)
+Suggerimento[word_List, stato_List, errors_List, score_Integer, gameMode_Integer] := Module[
+{
+	suggerimento, (* Una tra le lettere mancanti *)
+	lettereMancanti, (* Lista di lettere mancanti senza ripetizioni *)
+	newScore (* Punteggio aggiornato con la penalit\[AGrave] *)
+},
+	(* Calcola le lettere mancanti senza ripetizioni *)
 	lettereMancanti = DeleteDuplicates[Complement[word, stato]];
 	
-	(* Scelta casuale di una tra le lettere mancanti *)
+	(* Scelta casuale di una lettera mancante *)
 	suggerimento = RandomChoice[lettereMancanti];
 	
-	(* Sottrazione del punteggio per aver usato un suggerimento *)
+	(* Penalit\[AGrave]: -5 punti per ogni livello di difficolt\[AGrave]  *)
 	newScore = score - 5 * gameMode;
 	
-	(* Simulazione della scelta di una lettera con la lettera suggerita, chiamata con gameMode=0 per evitare che incrementi il punteggio *)
+	(* Applica il suggerimento aggiornando lo stato senza cambiare punteggio *)
 	AggiornaStato[word, stato, suggerimento, newScore, 0, errors]
 ];
 
 
-(* Implementazione della funzione Pulisci *)
-Pulisci[stato_List] :=
-Module[ {newState, errors = {}, score = 0},
+(* Reinizializza lo stato di gioco *)
+Pulisci[stato_List] := Module[
+{
+	newState, (* Vettore di trattini bassi quanti la lunghezza della parola *)
+	errors = {}, (* Errori *)
+	score = 0 (* Punteggio *)
+},
 	(* Reinizializza lo stato *)
 	newState = InizializzaStato[stato];
 	
@@ -93,37 +110,48 @@ Module[ {newState, errors = {}, score = 0},
 
 
 (* Funzioni ausiliarie *)
-(* Funzione booleana che verifica se in una stringa data sono presenti lettere accentate o apostrofi *)
-HaCaratteriNonAmmessiQ[s_] :=
-Module[ { accenti = {"\[AGrave]", "\[EGrave]", "\[IGrave]", "\[OGrave]", "\[UGrave]", "\[AAcute]", "\[EAcute]", "\[IAcute]", "\[OAcute]", "\[UAcute]"} },
+(* Verifica la presenza di caratteri accentati o apostrofi *)
+HaCaratteriNonAmmessiQ[s_] := Module[ 
+{ 
+	accenti = {"\[AGrave]", "\[EGrave]", "\[IGrave]", "\[OGrave]", "\[UGrave]", "\[AAcute]", "\[EAcute]", "\[IAcute]", "\[OAcute]", "\[UAcute]"} (* Vettore delle lettere accentate presenti nella lingua italiana *)
+},
 	StringContainsQ[s, Alternatives @@ accenti] || StringContainsQ[s, "'"]
 ];
 
-(* Funzione che data una lista caratteri ritorna una lista di "_" della stessa lunghezza della lista data *)
+(* Crea un array di trattini bassi della lunghezza della parola *)
 InizializzaStato[word_List] := ConstantArray["_", Length[word]]
 
-(* Funzione che aggiorna lo stato del gioco *)
-AggiornaStato[word_List, currentState_List, guess_, score_Integer, gameMode_, errors_List:{}] := 
-Module[{newState, newErrors, newScore},
+(* Aggiorna lo stato in base al tentativo dell'utente *)
+AggiornaStato[word_List, currentState_List, guess_, score_Integer, gameMode_, errors_List:{}] := Module[
+{
+	newState, (* Nuovo vettore che indica le lettere indovinate o trattini bassi *)
+	newErrors, (* Nuovo vettore con le lettere non presenti nella parola da indovinare *)
+	newScore (* Punteggio aggiornato in caso la lettera \[EGrave] stata indovinata *)
+},
 
 	If[MemberQ[word, guess],
-		(* Lettera correta: aggiorna currentState *)
+		(* Lettera corretta: aggiorno currentState e punteggio *)
 		newState = MapThread[If[#2 == guess, guess, #1] &, {currentState, word}];
 		newErrors = errors;
 		newScore = score + 10 * gameMode, (* In caso di lettera corretta per suggerimento gameMode = 0 *)
 		
-		(* Lettera sbagliata: aggiorna errors *)
+		(* Lettera sbagliata: mantengo lo stato e aggiungo l'errore *)
 		newState = currentState;
 		newErrors = Append[errors, guess];
 		newScore = score
 	];
 	
-	(* Ritorna lo stato, gli errori e lo score aggiornati *)
+	(* Ritorna un vettore con lo stato, gli errori e il punteggio aggiornati *)
 	{newState, newErrors, newScore}
 ];
 
-SalvaRecord[nome_String, punteggio_Integer, file_:"score.json"] := 
-Module[{record, datiEsistenti = {}, nuovoContenuto},
+(* Salva il record di punteggio in un file JSON *)
+SalvaRecord[nome_String, punteggio_Integer, file_:"score.json"] := Module[
+{
+	record, (* Record da memorizzare *)
+	datiEsistenti = {}, (* Record memorizzati precedentemente *)
+	nuovoContenuto (* Lista dei 10 migliori record ordinati in base al punteggio *)
+},
 	(* Nuovo record come associazione *)
 	record = {"nome" -> nome, "punteggio" -> punteggio};
   
@@ -140,21 +168,29 @@ Module[{record, datiEsistenti = {}, nuovoContenuto},
 	Export[file, nuovoContenuto, "JSON"];
 ];
 
-(* Funzione per caricare e ordinare la classifica *)
-RecuperaClassifica[file_:"score.json"] := 
-Module [{classifica = {}},
-	If[FileExistsQ[file], (* Controllo l'esistenza del file *)
+(* Carica e ordina la classifica dal file JSON *)
+RecuperaClassifica[file_:"score.json"] := Module [
+{
+	classifica = {} (* Lista ordinata dei record *)
+},
+	If[FileExistsQ[file], (* Verifica l'esistenza del file *)
 	classifica = Import[file, "JSON"]; (* Se il file esiste carica i suoi dati *)
 	If[!ListQ[classifica], classifica = {}], (* Se non ci sono dati inizializza classifica come lista vuota *)
 	classifica = {} (* Se non esite il file inizializza classifica come lista vuota *)
 	];
 	
-	(* Ritorna la classifica ordinata in maniera decrescente in base al punteggio *)
+	(* Ordina in ordine descrescente di punteggio *)
 	classifica = Reverse@SortBy[classifica, #[[2,2]] &] (* Usa l'operatore di partizione per accedere al valore del punteggio *)
 ];
 
-MostraClassificaGUI[score_Integer] := 
-DynamicModule[{nome = "", classifica = {}, punteggioSalvato = False, file = "score.json"},
+(* Interfaccia grafica per mostrare la classifica *)
+MostraClassificaGUI[score_Integer] := DynamicModule[
+{
+	nome = "", (* Nome da memorizzare nella classifica *)
+	classifica = {}, (* Lista ordinata dei record *)
+	punteggioSalvato = False, (* Flag per verificare se il punteggio \[EGrave] gi\[AGrave] stato salvato *)
+	file = "score.json" (* Nome del file dove viene memorizzata la classifica *)
+},
 	(* Inizializza classifica *)
 	SetDirectory[NotebookDirectory[]];
 	classifica = RecuperaClassifica[];
@@ -175,22 +211,24 @@ DynamicModule[{nome = "", classifica = {}, punteggioSalvato = False, file = "sco
 						Frame -> All,
 						Alignment -> Center
 						],
-					
 					If[
 						!punteggioSalvato,
 						Column[{
+								(* Campo per inserire il nome *)
 	                            "Inserisci il tuo nome:",
 	                            InputField[Dynamic[nome], String, FieldSize -> 20],
+	                            (* Pulsante per salvare il record *)
 		                            Button[
 		                                 "Salva Punteggio",
 										Module[{nomeVal = nome},
-		                                    Print[SalvaRecord[nomeVal, score]];
+		                                    SalvaRecord[nomeVal, score];
 		                                    classifica = RecuperaClassifica[];
 		                                    punteggioSalvato = True;
 		                                 ],
 		                                 Enabled -> Dynamic[StringLength[nome] > 0]
 		                            ]
 	                        }],
+	                        (* Pulsante per chiudere il pop-up *)
 	                        Button["Chiudi", DialogReturn[]]
 					]
 				}, Spacings -> 1.5]
@@ -205,6 +243,7 @@ DynamicModule[{nome = "", classifica = {}, punteggioSalvato = False, file = "sco
 ];
 
 
+(* Disegna l'impiccato in base al numero di errori *)
 DisegnaImpiccato[n_] := Graphics[
   {
     Thick,
@@ -232,6 +271,7 @@ righeTastiera = {
 };
 
 
+(* Genera l'interfaccia complete per il gioco *)
 GeneraInterfaccia[] := DynamicModule[
   {
     seed, (* Valore del seed per la generazione pseudo-casuale della parola *)
@@ -251,7 +291,7 @@ GeneraInterfaccia[] := DynamicModule[
 
       "selezione", (* Fase di selezione della difficolt\[AGrave] e del seed *)
       Column[{
-        Style["\|01f3afSeleziona la difficolt\[AGrave]", Bold, 16], , (* Titolo della sezione di selezione *)
+        Style["\|01f3afSeleziona la difficolt\[AGrave]", Bold, 16], (* Titolo della sezione di selezione *)
         RadioButtonBar[Dynamic[gamemode], {1 -> "Facile", 2 -> "Media", 3 -> "Difficile"}], (*Selezione difficolt\[AGrave]*)
         (* Controllo sull'input del seed su ogni carattere inserito *)
         Row[{"Seed (opzionale): ", InputField[
@@ -354,6 +394,7 @@ GeneraInterfaccia[] := DynamicModule[
                 ]        
               ],
               Button["\|01f9fdPulisci",
+              (* Non viene aggiornata classificaMostrata in modo da non poter salvare il punteggio dopo aver rigiocata la stessa parola *)
                 {stato, errori, score} = Pulisci[parola] (* Resetta lo stato del gioco con la funzione Pulisci *)
               ]
             }],
